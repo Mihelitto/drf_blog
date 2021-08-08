26021e2cf56dda2abc347b9780ed39ab4fceeaa2
Michael Yudin <mihelitto@gmail.com>
2021-08-08 18:27:23 +0300

    Fix admin urls

--- drf_blog/urls.py
+++ drf_blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
+from django.contrib import admin
 
 urlpatterns = [
     path('', include('blog.urls')),
+    path('admin/', admin.site.urls),
     path('api/', include('rest_framework.urls', namespace='rest_framework'))
 ]
