a05e9478567293413d4f99c656146bef35197380
Michael Yudin <mihelitto@gmail.com>
2021-08-09 01:46:07 +0300

    Refactor urls

--- drf_blog/urls.py
+++ drf_blog/urls.py
@@ -4,5 +4,4 @@ from django.contrib import admin
 urlpatterns = [
     path('', include('blog.urls')),
     path('admin/', admin.site.urls),
-    path('api/', include('rest_framework.urls', namespace='rest_framework'))
 ]
