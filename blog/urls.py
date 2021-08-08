062f0a8e4d737413d2953c760b729572447c2809
Michael Yudin <mihelitto@gmail.com>
2021-08-09 01:25:27 +0300

    Modify comment api to return comments tree

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,10 @@ from blog import views
 
 router = routers.DefaultRouter()
 router.register(r'posts', views.PostViewSet)
-router.register(r'comments', views.CommentViewSet)
 
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('comments/', views.CommentList.as_view()),
+    path('comments/<int:pk>', views.CommentsTree.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
@@ -9,7 +11,43 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
 
 
-class CommentViewSet(viewsets.ModelViewSet):
+class CommentsTree(APIView):
+    def get(self, request, pk):
+        current_comment = Comment.objects.get(pk=pk)
+        current_depth = current_comment.depth
+        current_post = current_comment.post
+        if current_depth < 3:
+            comments = Comment.objects.filter(
+                post=current_post,
+                depth__range=(current_depth, current_depth+3)
+            )
+        else:
+            comments = Comment.objects.filter(
+                post=current_post,
+                depth__gt=current_depth
+            )
+        result_comments = {pk: current_comment}
+        for comment in comments:
+            if comment.parent_id in result_comments.keys():
+                result_comments[comment.id] = comment
 
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
+        serializer = CommentSerializer(result_comments.values(), many=True)
+        return Response({"comment": serializer.data})
+
+
+class CommentList(APIView):
+    def get(self, request):
+        comment = Comment.objects.all()
+        serializer = CommentSerializer(comment, many=True)
+        return Response({"comment": serializer.data})
+
+    def post(self, request):
+        comment = request.data.get('comment')
+        serializer = CommentSerializer(data=comment)
+        if serializer.is_valid(raise_exception=True):
+            comment_saved = serializer.save()
+        return Response({
+            "success":
+                f"Comment from '{comment_saved.author}' created successfully"
+        })
+
