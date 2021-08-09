28a279953d332354c2ef36f620e1397ff3da6e78
Michael Yudin <mihelitto@gmail.com>
2021-08-09 18:19:03 +0300

    Update urls to make REST like

--- blog/urls.py
+++ blog/urls.py
@@ -4,11 +4,11 @@ from blog import views
 
 router = routers.DefaultRouter()
 router.register(r'posts', views.PostViewSet)
+router.register(r'comments', views.CommentViewSet)
 
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('comments/', views.CommentList.as_view()),
-    path('comments/<int:pk>', views.CommentsTree.as_view()),
-    path('post_comments/<int:pk>', views.PostCommentsTree.as_view()),
+    path('posts/<int:post_pk>/comments/', views.PostCommentsTree.as_view()),
+    path('posts/<int:post_pk>/comments/<int:comment_pk>', views.CommentsTree.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -11,53 +12,44 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
 
 
+class CommentViewSet(viewsets.ModelViewSet):
+
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
 class CommentsTree(APIView):
-    def get(self, request, pk):
-        current_comment = Comment.objects.get(pk=pk)
+    def get(self, request, post_pk, comment_pk):
+        try:
+            current_comment = Comment.objects.get(pk=comment_pk, post_id=post_pk)
+        except Comment.DoesNotExist:
+            raise Http404
         current_depth = current_comment.depth
-        current_post = current_comment.post
         if current_depth < 3:
             comments = Comment.objects.filter(
-                post=current_post,
+                post_id=post_pk,
                 depth__range=(current_depth, current_depth+3)
             )
         else:
             comments = Comment.objects.filter(
-                post=current_post,
+                post_id=post_pk,
                 depth__gt=current_depth
             )
-        result_comments = {pk: current_comment}
+        result_comments = {comment_pk: current_comment}
         for comment in comments:
             if comment.parent_id in result_comments.keys():
                 result_comments[comment.id] = comment
 
         serializer = CommentSerializer(result_comments.values(), many=True)
-        return Response({"comment": serializer.data})
-
-
-class CommentList(APIView):
-    def get(self, request):
-        comment = Comment.objects.all()
-        serializer = CommentSerializer(comment, many=True)
-        return Response({"comment": serializer.data})
-
-    def post(self, request):
-        comment = request.data.get('comment')
-        serializer = CommentSerializer(data=comment)
-        if serializer.is_valid(raise_exception=True):
-            comment_saved = serializer.save()
-        return Response({
-            "success":
-                f"Comment from '{comment_saved.author}' created successfully"
-        })
+        return Response({"comments": serializer.data})
 
 
 class PostCommentsTree(APIView):
-    def get(self, request, pk):
+    def get(self, request, post_pk):
         comments = Comment.objects.filter(
-                post_id=pk,
+                post_id=post_pk,
                 depth__lte=3
         )
 
         serializer = CommentSerializer(comments, many=True)
-        return Response({"comment": serializer.data})
+        return Response({"comments": serializer.data})
